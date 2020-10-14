73a52f1bdc85645c2a632e2e492166cf3c80a6cc
utkarsh8021 <mishra8021@gmail.com>
2020-10-15 01:23:04 +0530

    ..

--- app.js
+++ app.js
@@ -1,41 +1,56 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const mongoose = require("mongoose");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+var express = require("express");
 var app = express();
+const path = require("path");
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+// const cors = require("cors");
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Load Public folder
+app.use(express.static("public"));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// index page
+app.get("/", function (req, res) {
+  res.render("index.ejs");
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// about page
+app.get("/about", function (req, res) {
+  res.render("about.ejs");
 });
 
-module.exports = app;
+// projects page
+app.get("/projects", function (req, res) {
+  res.render("project.ejs");
+});
+
+// services page
+app.get("/services", function (req, res) {
+  res.render("services.ejs");
+});
+
+// contact page
+app.get("/contact", function (req, res) {
+  res.render("contact.ejs");
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://portfolio:qwerty@1234@cluster0.70zvn.mongodb.net/contact?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    }
+  )
+  .then(() => {
+    console.log("DB CONNECTED");
+  });
+
+// Start Server
+app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
